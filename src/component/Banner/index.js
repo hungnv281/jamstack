5c7edc958bd7dec4049c3d96d15022b2aa819fe3
hoangtph <hung.nv281@gmail.com>
2022-11-18 06:38:52 +0700

    add scroll, fade in animate

--- src/component/Banner/index.js
+++ src/component/Banner/index.js
@@ -1,26 +1,11 @@
-import React from "react";
-import styles from "./styles.module.scss";
-import Navbar from "../Navbar";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
 import { Button } from "antd";
+import React from "react";
+import "react-slideshow-image/dist/styles.css";
+import Navbar from "../Navbar";
+import styles from "./styles.module.scss";
 // import { Button } from "antd";
 
 function Banner() {
-  const slideImages = [
-    {
-      url: "images/slide_1.jpg",
-      caption: "Slide 1",
-    },
-    {
-      url: "images/slide_2.jpg",
-      caption: "Slide 2",
-    },
-    {
-      url: "images/slide_3.jpg",
-      caption: "Slide 3",
-    },
-  ];
   return (
     <div className={styles.container}>
       <Navbar />
